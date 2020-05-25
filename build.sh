3d8ab15d6066a461ed67b708631bfd73de3900ed
Zachary Wasserman <zachwass2000@gmail.com>
2020-05-25 09:52:59 -0700

    Update to 4.3.0

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ build_rpm() {
     docker build -f rpm-dockerfile . --build-arg OSQUERY_URL=https://pkg.osquery.io/rpm/osquery-${VERSION}-1.linux.x86_64.rpm --build-arg OS_IMAGE=$OS -t dactiv/osquery:${VERSION}-${TAG}
 }
 
-versions='4.1.2 4.1.1 4.0.2 3.3.2 3.3.0 3.2.6 3.2.4'
+versions='4.3.0'
 deb_platforms='ubuntu:20.04 ubuntu:18.04 ubuntu:16.04 ubuntu:14.04'
 rpm_platforms='centos:centos8 centos:centos7 centos:centos6'
 
